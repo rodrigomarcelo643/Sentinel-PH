5d182967e8b0aee7a535affc73e5e102e089ba93
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-02-23 00:48:54 +0800

    fix(path): move Landing Page to pages instead of components

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "@/components/LandingPage";
+import LandingPage from "@/pages/public/LandingPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<div className="pt-20 p-8">About Page</div>} />
+      <Route
+        path="/about"
+        element={<div className="pt-20 p-8">About Page</div>}
+      />
       <Route path="/map" element={<div className="pt-20 p-8">Map Page</div>} />
-      <Route path="/region/:regionId" element={<div className="pt-20 p-8">Region Page</div>} />
-      <Route path="/signin" element={<div className="pt-20 p-8">Sign In Page</div>} />
-      <Route path="/register" element={<div className="pt-20 p-8">Register Page</div>} />
+      <Route
+        path="/region/:regionId"
+        element={<div className="pt-20 p-8">Region Page</div>}
+      />
+      <Route
+        path="/signin"
+        element={<div className="pt-20 p-8">Sign In Page</div>}
+      />
+      <Route
+        path="/register"
+        element={<div className="pt-20 p-8">Register Page</div>}
+      />
     </Routes>
   );
 }
