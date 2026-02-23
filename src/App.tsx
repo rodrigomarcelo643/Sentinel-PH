8b961b2b0f1ac92dbf54d44161f142f4229b0888
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-02-23 10:43:59 +0800

    feat: add framer animations and loading state for loading animation if (state=loading)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar";
+import AppRoutes from "./router";
+import LoadingAnimation from "./components/LoadingAnimation";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <AnimatePresence mode="wait">
+        {loading ? (
+          <LoadingAnimation key="loading" />
+        ) : (
+          <>
+            <Navbar />
+            <AppRoutes />
+          </>
+        )}
+      </AnimatePresence>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
