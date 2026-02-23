5092e79171d780e0157336d24f279476aabf4dec
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-02-23 18:20:22 +0800

    feat: add firebase service account credentials needed

--- backend/config/firebase-admin.ts
+++ backend/config/firebase-admin.ts
@@ -3,10 +3,18 @@ import * as admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
+      type: 'service_account',
       projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      clientId: process.env.FIREBASE_CLIENT_ID,
+      authUri: process.env.FIREBASE_AUTH_URI,
+      tokenUri: process.env.FIREBASE_TOKEN_URI,
+      authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+      universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN || 'googleapis.com',
+    } as admin.ServiceAccount),
     databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   });
 }
