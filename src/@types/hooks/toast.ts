8087d27d6c5e4a49c51d0462bf0ee306f521c37b
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-03-13 14:37:42 +0800

    feat: define toast hook types and interfaces

---
+++ src/@types/hooks/toast.ts
@@ -0,0 +1,42 @@
+// Types from src/hooks/use-toast.ts
+
+import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
+import * as React from "react";
+
+export type ToasterToast = ToastProps & {
+  id: string;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  action?: ToastActionElement;
+};
+
+export type ActionType = {
+  ADD_TOAST: "ADD_TOAST";
+  UPDATE_TOAST: "UPDATE_TOAST";
+  DISMISS_TOAST: "DISMISS_TOAST";
+  REMOVE_TOAST: "REMOVE_TOAST";
+};
+
+export type Action =
+  | {
+      type: ActionType["ADD_TOAST"];
+      toast: ToasterToast;
+    }
+  | {
+      type: ActionType["UPDATE_TOAST"];
+      toast: Partial<ToasterToast>;
+    }
+  | {
+      type: ActionType["DISMISS_TOAST"];
+      toastId?: ToasterToast["id"];
+    }
+  | {
+      type: ActionType["REMOVE_TOAST"];
+      toastId?: ToasterToast["id"];
+    };
+
+export interface State {
+  toasts: ToasterToast[];
+}
+
+export type Toast = Omit<ToasterToast, "id">;
