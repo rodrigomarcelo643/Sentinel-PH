c2c2829c10843fec4d59a9c6bd48d4fa6ecb4f8f
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-03-26 15:17:51 +0800

    feat: add types role

--- src/@types/contexts/auth.ts
+++ src/@types/contexts/auth.ts
@@ -24,7 +24,7 @@ export interface User {
 export interface AuthContextType {
   user: User | null;
   loading: boolean;
-  login: (email: string, password: string) => Promise<void>;
+  login: (email: string, password: string) => Promise<any>;
   signup: (email: string, password: string) => Promise<void>;
   logout: () => Promise<void>;
   updateUserProfile: (updates: Partial<User>) => void;
